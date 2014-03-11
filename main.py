1bb995cd75912dd151a80cfa87f42edc3d3e9ffa
Nicholas Petty <nicholaspetty@Karens-MacBook-Pro.local>
2014-03-11 18:08:35 -0500

    Update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import time
 
 render = web.template.render('templates/')
-urls = ('/', 'index', '/link/(.*)', 'Link', '/rm/(.*)', 'rm')
+urls = ('/', 'index', '/link/(.*)', 'Link', '/a/(.*)', 'a', '/about', 'about')
 linkDict = {}
 
 class index:
@@ -14,14 +14,14 @@ class index:
 		self.render = web.template.render('templates/')
 
 	def GET(self):
-		form = '<form method="POST" enctype="multipart/form-data" action=""><input type="file" name="myfile" /><input type="submit" /></form>'
+		form = '<form method="POST" enctype="multipart/form-data" action=""><input type="file" name="myfile" onchange="this.form.submit();"/>'
 		return self.render.index(form)
 
 	def POST(self):
 		x = web.input(myfile={})
 		filename = (x['myfile'].filename)
 		what = (x['myfile'].file.read()) 
-		submittedFile = open('static/store/' + filename, 'wb')
+		submittedFile = open('store/' + filename, 'wb')
 		submittedFile.write(what)
 		submittedFile.close()
 		return self.linkGen(filename)
@@ -36,28 +36,43 @@ class index:
 			link = randint(2,500) * len(filename)
 
 		linkDict[link] = filename
-		httpLink = 'http://localhost:8080/link/' + str(link)
-		return self.render.index(httpLink)
+		raise web.seeother('/a/' + str(link))
 
 class Link:
 	def __init__(self):
 		self.render = web.template.render('templates/')
 
 	def GET(self, link):
-		if os.path.isfile('static/store/' + linkDict[int(str(link))]):
-			link = link.strip('link/')
-			request = '../static/store/' + linkDict[int(str(link))]
-			#f = open(request, 'rb').read()
-			return self.render.link(link=request, linkNum=link)
-			#os.remove(request)
-			#del linkDict[int(str(link))]
+		link = link.strip('link/')
+		if int(str(link)) in linkDict.keys():
+			request = 'store/' + linkDict[int(str(link))]
+			f = open(request, 'rb')
+			web.header('Content-Type','application/binary')
+			web.header('Content-Disposition','attachment; filename="' + linkDict[int(str(link))] + '"')
+			while True:
+				buf = f.read(1024)
+				if len(buf) == 0:
+					break
+				yield buf
+			f.close()
+			os.remove(request)
+			del linkDict[int(str(link))]
 		else:
-			return self.render.index('Nothing here...')
+			yield self.render.index('Nothing here...')
 
-class rm:
-	def __init__(self, linkURL):
-		print 'called'
-		print linkURL
+class a:
+	def __init__(self):
+		self.render = web.template.render('templates/')
+
+	def GET(self, value):
+		return self.render.link('http://localhost:8080/link/' + str(value))
+
+class about:
+	def __init__(self):
+		self.render = web.template.render('templates/')
+
+	def GET(self):
+		return self.render.about()
 
 if __name__ == "__main__":
 	app = web.application(urls, globals()) 
