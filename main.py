4d8fc89b31036361940696909e5d4a3986a6175b
Nicholas Petty <nick@ihackeverything.com>
2014-03-15 18:55:57 -0500

    Update
    Added new GIF logo, Upload file size limit, sql connectivity

--- main.py
+++ main.py
@@ -4,27 +4,37 @@ import datetime
 from random import randint
 import os
 import time
+import MySQLdb
+
+configFile = open('config', 'r')
+config = {}
+for line in configFile:
+	line = line.split(':')
+	config[line[0]] = line[1].strip('\n')
+
+db = MySQLdb.connect(host=config['dbHost'], user=config['dbUser'], passwd=config['dbPass'], db=config['db'])
 
 render = web.template.render('templates/')
 urls = ('/', 'index', '/link/(.*)', 'Link', '/a/(.*)', 'a', '/about', 'about', '/download/(.*)', 'Download')
-linkDict = {}
 
 class index:
 	def __init__(self):
 		self.render = web.template.render('templates/')
 
 	def GET(self):
-		form = '<form method="POST" enctype="multipart/form-data" action=""><input type="file" name="myfile" onchange="this.form.submit();"/>'
+		#form = '<form method="POST" enctype="multipart/form-data" action=""><input type="file" name="myfile" onchange="this.form.submit();"/>'
+		form = '<form id="uploadForm" method="POST" enctype="multipart/form-data" action=""><input type="file" id="uploadedFile" name="myfile" /><input type="submit" /><br><em>Max upload size: 10mb</em>'
 		return self.render.index(form)
 
 	def POST(self):
 		x = web.input(myfile={})
 		filename = (x['myfile'].filename)
-		what = (x['myfile'].file.read()) 
-		submittedFile = open('store/' + filename, 'wb')
-		submittedFile.write(what)
-		submittedFile.close()
-		return self.linkGen(filename)
+		what = (x['myfile'].file.read())
+		if len(what) < 10485760: 
+			submittedFile = open('store/' + filename, 'wb')
+			submittedFile.write(what)
+			submittedFile.close()
+			return self.linkGen(filename)
 
 	def linkGen(self, filename):
 		i = datetime.datetime.now()
@@ -35,7 +45,9 @@ class index:
 		else:
 			link = randint(2,500) * len(filename)
 
-		linkDict[link] = filename
+		with db:
+			cur = db.cursor()
+			cur.execute("INSERT INTO files VALUES (%s, %s)", (filename, link))
 		raise web.seeother('/a/' + str(link))
 
 class Link:
@@ -65,24 +77,31 @@ class Download:
 		self.render = web.template.render('templates/')
 
 	def GET(self, link):
-		print 'called'
 		link = link.strip('link/')
-		if int(str(link)) in linkDict.keys():
-			print 'key found'
-			request = 'store/' + linkDict[int(str(link))]
-			f = open(request, 'rb')
-			web.header('Content-Type','application/binary')
-			web.header('Content-Disposition','attachment; filename="' + linkDict[int(str(link))] + '"')
-			while True:
-				buf = f.read(1024)
-				if len(buf) == 0:
-					break
-				yield buf
-			f.close()
-			os.remove(request)
-			del linkDict[int(str(link))]
-		else:
-			yield self.render.index('Nothing here...')
+		link = int(link)
+		with db:
+			cur = db.cursor()
+
+			if cur.execute("SELECT filename FROM files WHERE link={0}".format(link)):
+				row = cur.fetchone()
+				filename = row[0]
+				request = 'store/' + filename
+				
+				f = open(request, 'rb')
+				
+				web.header('Content-Type','application/binary')
+				web.header('Content-Disposition','attachment; filename="' + filename + '"')
+				
+				while True:
+					buf = f.read(1024)
+					if len(buf) == 0:
+						break
+					yield buf
+				f.close()
+				os.remove(request)
+				cur.execute("DELETE FROM files WHERE link={0}".format(link))
+			else:
+				yield self.render.index('Nothing here...')
 
 if __name__ == "__main__":
 	app = web.application(urls, globals()) 
